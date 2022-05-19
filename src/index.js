37d0e00952c1505b94b270e5be3c0da3ba1f8727
sanket-walanj <code.sanky@gmail.com>
2022-05-19 14:10:54 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => (
+  <img
+    src='https://images-eu.ssl-images-amazon.com/images/I/41+grDTP2FL._AC_UL604_SR604,400_.jpg'
+    alt=''
+  />
 );
+const Title = () => {
+  return <h1>DO EPIC SHIT</h1>;
+};
+const Author = () => {
+  return <h4>Ankur Warikoo</h4>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<BookList />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
